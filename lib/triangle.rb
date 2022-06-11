52f4d1ef98842fa78d8e24489217cc3eef6b1abb
Catherine Kawara <ndutakawara2@gmail.com>
2022-06-11 13:44:03 +0300

    commit tree 94887ca3d64472bd0e2e6bc80ada00eed59167a0

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :side1
+  attr_accessor :side2
+  attr_accessor :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if (side1==side2 && side2==side3)
+      :equilateral
+    elsif (side1==side2 || side1==side3 || side2==side3)
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def valid_triangle?
+    (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) && ([side1, side2, side3].all?(&:positive?))
+  end
+  def validate_triangle
+    raise TriangleError unless valid_triangle?
+  end
+
+  class TriangleError < StandardError
+  end
 end
